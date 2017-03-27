5a5bf161acdc1b72d33e7b7c9c076e2686b21cff
razeltif <razeltif@gmail.com>
2017-03-27 23:20:13 +0200

    et la lumière fût
    ainsi que quelques correction au niveau de l'emplacement des objets dans la scène, maintenant c'est plus intuitif (et donc mieux)

--- funct.c
+++ funct.c
@@ -54,45 +54,72 @@ void cube(int x,int y,int z,int scalex, int scaley, int scalez,float col1,float 
 
  void cubev2(int x,int y,int z,int scalex, int scaley, int scalez,float col1,float col2,float col3){
 
+   // Proprietes de l'objet (sa matiere)
+   //
+   GLfloat mat_ambient[] = { 0.7, 0.7, 0.7, 1.0 };
+   GLfloat mat_diffuse[] = { 0.0, 1.0, 0.4, 1.0 };
+   GLfloat mat_specular[] = { 0.5, 0.5, 0.5, 1 };
+   GLfloat mat_shininess[] = { 70.0 };
+   GLfloat mat_emission[] = {0, 0, 0, 0};
+
+   glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+   glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+
+
 
     glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
     glColor3f (col1, col2, col3);
+    //FRONT
     glBegin(GL_POLYGON);
+       glNormal3f(0.0f, 0.0f, 1.0f);
        glVertex3f (x,   y,   z  );
        glVertex3f (x+scalex, y,   z  );
        glVertex3f (x+scalex, y+scaley, z  );
        glVertex3f (x,   y+scaley, z  );
     glEnd();
 
+    //BOTTOM
     glBegin(GL_POLYGON);
+       glNormal3f(1.0f, 0.0f, 1.0f);
        glVertex3f (x,   y,   z  );
        glVertex3f (x+scalex, y,   z  );
        glVertex3f (x+scalex, y,   z+scalez);
        glVertex3f (x,   y,   z+scalez);
     glEnd();
 
+    //BACK
     glBegin(GL_POLYGON);
+       glNormal3f(0.0f, 0.0f, -1.0f);
        glVertex3f (x,   y,   z+scalez);
        glVertex3f (x+scalex, y,   z+scalez);
        glVertex3f (x+scalex, y+scaley, z+scalez);
        glVertex3f (x,   y+scaley, z+scalez);
     glEnd();
 
+    //RIGHT
     glBegin(GL_POLYGON);
+       glNormal3f(1.0f, 0.0f, 0.0f);
        glVertex3f (x+scalex, y,   z  );
        glVertex3f (x+scalex, y+scaley, z  );
        glVertex3f (x+scalex, y+scaley, z+scalez);
        glVertex3f (x+scalex, y,   z+scalez);
     glEnd();
 
+    //UP
     glBegin(GL_POLYGON);
+       glNormal3f(1.0f, 0.0f, 1.0f);
        glVertex3f (x  , y+scaley, z  );
        glVertex3f (x+scalex, y+scaley, z  );
        glVertex3f (x+scalex, y+scaley, z+scalez);
        glVertex3f (x  , y+scaley, z+scalez);
     glEnd();
 
+    //LEFT
     glBegin(GL_POLYGON);
+       glNormal3f(-1.0f, 0.0f, 0.0f);
        glVertex3f (x  , y,   z  );
        glVertex3f (x  , y+scaley, z  );
        glVertex3f (x  , y+scaley, z+scalez);
@@ -104,10 +131,27 @@ void cube(int x,int y,int z,int scalex, int scaley, int scalez,float col1,float 
 
 void triangle(float *p1,float *p2,float *p3,int scalex, int scaley, int scalez,float col1,float col2,float col3){
 
+  GLfloat mat_ambient[] = { col1, col2, col3, 1.0 };
+  GLfloat mat_diffuse[] = { col1, col2, col3, 1.0 };
+  GLfloat mat_specular[] = { 0.5, 0.5, 0.5, 1 };
+  GLfloat mat_shininess[] = { 70.0 };
+  GLfloat mat_emission[] = {0, 0, 0, 0};
+
+  glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+  glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+
+
+
    glColor3f (col1, col2, col3);
    glBegin(GL_POLYGON);
+      glNormal3f (p1[0], p1[1], p1[2]);
       glVertex3f (p1[0], p1[1], p1[2]);
+      glNormal3f (p2[0], p2[1], p2[2]);
       glVertex3f (p2[0], p2[1], p2[2]);
+      glNormal3f (p3[0], p3[1], p3[2]);
       glVertex3f (p3[0], p3[1], p3[2]);
    glEnd();
 
@@ -147,10 +191,14 @@ void triangle_rec(float *p1,float *p2,float *p3,int scalex, int scaley, int scal
 	calcul_normal(p5);
 	calcul_normal(p6);
 
-	triangle_rec(p1,p4,p5,scalex,scaley,scalez,col1,col2,col3, rec-1);
+	/*triangle_rec(p1,p4,p5,scalex,scaley,scalez,col1,col2,col3, rec-1);
 	triangle_rec(p4,p2,p6,scalex,scaley,scalez,col1+0.02,col2+0.02,col3+0.02,rec-1);
 	triangle_rec(p4,p5,p6,scalex,scaley,scalez,col1+0.04,col2+0.04,col3+0.04,rec-1);
-	triangle_rec(p5,p6,p3,scalex,scaley,scalez,col1+0.06,col2+0.06,col3+0.06,rec-1);
+	triangle_rec(p5,p6,p3,scalex,scaley,scalez,col1+0.06,col2+0.06,col3+0.06,rec-1);*/
+  triangle_rec(p1,p4,p5,scalex,scaley,scalez,col1,col2,col3, rec-1);
+  triangle_rec(p4,p2,p6,scalex,scaley,scalez,col1,col2,col3,rec-1);
+  triangle_rec(p4,p5,p6,scalex,scaley,scalez,col1,col2,col3,rec-1);
+  triangle_rec(p5,p6,p3,scalex,scaley,scalez,col1,col2,col3,rec-1);
 
 	}else{
 		triangle(p1,p2,p3,scalex, scaley, scalez,col1,col2,col3);
--- projet.c
+++ projet.c
@@ -11,7 +11,7 @@
 
 //position de la camera
 float ex=0;
-float ey=0;
+float ey=5;
 float ez=-5;
 
 //pas de déplacement de la caméra utilisé dans fct keyboard
@@ -22,20 +22,12 @@ float sphereTranslateX = 0;
 float sphereTranslateY = 0;
 float sphereTranslateZ = 0;
 
-// Proprietes de l'objet (sa matiere)
-//
-GLfloat mat_ambient[] = { 0.7, 0.7, 0.7, 1.0 };
-GLfloat mat_diffuse[] = { 0.1, 0.1, 0.1, 1.0 };
-GLfloat mat_specular[] = { 1, 1, 1, 1 };
-GLfloat mat_shininess[] = { 70.0 };
-GLfloat mat_emission[] = {0, 0, 0, 0};
-
 // Proprietes de la source lumineuse
 //
-GLfloat light_ambient[] = { 0.4, 0.4, 0.4, 0 };
-GLfloat light_diffuse[] = { 0.3, 0.3, 0.3, 0 };
+GLfloat light_ambient[] = { 0.4, 0.4, 0.4, 1 };
+GLfloat light_diffuse[] = { 0.3, 0.3, 0.3, 1 };
 GLfloat light_specular[] = { .9, .9,.9, 0 };
-GLfloat light_position[] = { -7.0, 2.0, 5.0, 1.0 };
+GLfloat light_position[] = { 2.0, 10.0, 2.0, 1.0 };
 
 // Proprietes generales de l'eclairage
 //
@@ -126,16 +118,16 @@ void SpecialInput(int key, int x, int y)
 {
   switch(key)
     {
-    case GLUT_KEY_UP:
+    case GLUT_KEY_LEFT:
       if(sphereTranslateX < 6) sphereTranslateX=sphereTranslateX+2;
     break;
-    case GLUT_KEY_DOWN:
+    case GLUT_KEY_RIGHT:
       if(sphereTranslateX > 0) sphereTranslateX=sphereTranslateX-2;
     break;
-    case GLUT_KEY_LEFT:
+    case GLUT_KEY_DOWN:
       if(sphereTranslateZ > 0) sphereTranslateZ = sphereTranslateZ-2;
     break;
-    case GLUT_KEY_RIGHT:
+    case GLUT_KEY_UP:
       if(sphereTranslateZ < 6) sphereTranslateZ=sphereTranslateZ+2;
     break;
   }
@@ -144,10 +136,9 @@ void SpecialInput(int key, int x, int y)
 void display(void)
 {
   int i;
-  float col = 0;
 
   glLoadIdentity ();
-  gluLookAt (ex, ey, ez, 4.0, 4.0, 4.0, 0.0, 1.0, 0.0);
+  gluLookAt (ex, ey, ez, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
   glClear(GL_COLOR_BUFFER_BIT);
   glClear(GL_DEPTH_BUFFER_BIT);
 
@@ -161,28 +152,20 @@ void display(void)
   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
   glEnable(GL_LIGHT0);
 
-  glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-  glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-  glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
-
-  glPushMatrix();
+  //glPushMatrix();
   //glRotatef(xRotation, 1, 0, 0);
 
   glScalef(1,1,1);
 
-  cubev2(0,0,0, 8,1,8, 0.2,0.5,0.4);
+  cubev2(-4,0,0, 8,1,8, 0.0,0.0,1.0);
 
-  glTranslatef(sphereTranslateX+1, sphereTranslateY+1 ,sphereTranslateZ+1);
+  glTranslatef(sphereTranslateX-3, sphereTranslateY+2 ,sphereTranslateZ+1);
   glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
-  //glTranslatef(1,1,0);
   for(i = 0; i < 20; i++){
-    triangle_rec(sommets[sindex[i][0]], sommets[sindex[i][1]], sommets[sindex[i][2]], 2,2,2, col,col,col,2);
-    col = col + 0.05;
+    triangle_rec(sommets[sindex[i][0]], sommets[sindex[i][1]], sommets[sindex[i][2]], 2,2,2, 0.0,1.0,0.0,2);
   }
 
-  glPopMatrix();
+//  glPopMatrix();
   glutSwapBuffers();
 }
 
